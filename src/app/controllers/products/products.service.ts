07f86077a8ea52edcd408ea8e64bd5bed2aece6e
galaxyangelz <nguyenhoangminhduc@gmail.com>
2017-04-15 00:22:02 +0300

    v1.0.2
    Product page function

---
+++ src/app/controllers/products/products.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
+
+import { contentHeaders } from '../../components/common/headers';
+import { url } from '../../components/common/headers';
+
+@Injectable()
+export class ProductsService {
+  constructor(private http: Http) { }
+
+  //GET products
+  getProducts() {
+  	return this.http.get(url+'products')
+      .map((res: Response) => res.json());
+  }
+
+  //POST products
+  postProducts(userId, typeId, name, desc, price) {
+  	let body = JSON.stringify({userId, typeId, name, desc, price});
+  	this.http.post(url+'products', body, {headers: contentHeaders});
+  }
+
+  //DELETE products
+  deleteProducts() {
+    this.http.delete(url+'products');
+  }
+
+  //GET products by Type
+  getProductsByType(typeId) {
+    return this.http.get(url+'products/type/'+typeId)
+      .map((res: Response) => res.json());
+  }
+
+  //DELETE products by type
+  deleteProductsByType(typeId) {
+    this.http.delete(url+'products/type/'+typeId);
+  }
+
+  //GET products by id
+  getProductById(productId) {
+    return this.http.get(url+'products/'+productId)
+      .map((res: Response) => res.json());
+  }
+
+  //UPDATE product by Id
+  editProductById(productId, typeId, name, desc, price) {
+    let body = JSON.stringify({typeId, name, desc, price});
+    this.http.put(url+'products/'+productId, body, {headers: contentHeaders});
+  }
+
+  //DELETE product by Id
+  deleteProductById(productId) {
+    this.http.delete(url+'products/'+productId);
+  }
+
+  //GET products by User
+  getProductsByUser() {
+    return this.http.get(url+'products/user/items')
+      .map((res: Response) => res.json());
+  }
+}
